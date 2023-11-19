8a7484f7c1e8673aa08047edb1c2b62819a83458
Sunmarine <Sunmarine777@yandex.ru>
2023-11-19 15:50:59 +0300

    Even number detection

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter any number ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 0)
+{
+      Console.WriteLine("Yes");
+}
+else
+{
+      Console.WriteLine("No");
+}
+
+/*Example1
+int a = 5;
+
+if ( a % 2 == 0)
+{
+      Console.WriteLine("Yes");
+}
+else
+{
+      Console.WriteLine("No");
+}*/
