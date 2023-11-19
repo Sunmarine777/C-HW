7be22e2a2dcb06ca077c779b8d8a6bf89979b7af
Sunmarine <Sunmarine777@yandex.ru>
2023-11-19 14:47:50 +0300

    Find max

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,11 +1,35 @@
-﻿int a = 5;
-int b = 6;
-int c = 7;
+﻿// Example1
+int a = 5;
+int b = 7;
 
 int max = a;
 
 if (a > max) max = a;
 if (b > max) max = b;
-if (c > max) max = c;
 
-Console.WriteLine(max);
+Console.Write("max = ");
+Console.Write(max);
+
+/* Example2
+int a = 2;
+int b = 10;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.Write(max);*/
+
+/* Example3
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.Write(max);*/
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 6;
+int c = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
