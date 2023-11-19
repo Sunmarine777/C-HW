aacfbf9e8a678427a922ec45dd4ad793d8ee1c34
Sunmarine <Sunmarine777@yandex.ru>
2023-11-19 15:03:32 +0300

    Max vs Min commited

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,33 +3,50 @@ int a = 5;
 int b = 7;
 
 int max = a;
+int min = a;
 
 if (a > max) max = a;
 if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
 
-Console.Write("max = ");
 Console.Write(max);
+Console.Write(" is bigger and ");
+Console.Write(min);
+Console.Write(" is smaller");
 
-/* Example2
+/*Example2
 int a = 2;
 int b = 10;
 
 int max = a;
+int min = a;
 
 if (a > max) max = a;
 if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
 
 Console.Write("max = ");
-Console.Write(max);*/
+Console.WriteLine(max);
+Console.WriteLine("and");
+Console.Write("min = ");
+Console.Write(min);
 
-/* Example3
+Example3
 int a = -9;
 int b = -3;
 
 int max = a;
+int min = a;
 
 if (a > max) max = a;
 if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
 
 Console.Write("max = ");
-Console.Write(max);*/
+Console.WriteLine(max);
+Console.WriteLine("and");
+Console.Write("min = ");
+Console.Write(min); */
